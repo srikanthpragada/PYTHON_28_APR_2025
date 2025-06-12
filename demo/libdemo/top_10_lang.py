48fe11a84e1123433a2f925341b2861a7e6b42f8
srikanthpragada <srikanthpragada@gmail.com>
2025-06-12 19:55:38 +0530

    12-JUN

--- demo/libdemo/top_10_lang.py
+++ demo/libdemo/top_10_lang.py
@@ -1,5 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
 
 resp = requests.get("https://www.tiobe.com/tiobe-index/")
+bs = BeautifulSoup(resp.text, "html.parser")
 
-print(resp.text)
+table = bs.find(id = "top20")
+body = table.tbody
+# Take only first 10 rows
+rows = body.find_all("tr")[:10]
+
+
+for row in rows:
+    # take all cols
+    cols = row.find_all("td")
+    print(f"{cols[4].text:20}  {cols[5].text}")
